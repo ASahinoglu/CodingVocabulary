136785ea85f64f651fb1febffe96ed4e536f0006
ASahinoglu <aydinsahinoglu888@gmail.com>
2025-06-16 15:52:38 +0200

    PVQ first commit

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print("Willkommen beim Programmier-Vokabel-Quiz!");
+
+  Map<String, String> words = {
+    "Absturz": "Crash",
+    "Laufzeit": "Runtime",
+    "Speicher": "Memory",
+    "Zeiger": "Pointer",
+    "Übersetzer": "Compiler",
+    "Fehler": "Bug",
+  };
+}
